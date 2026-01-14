fe4f8485033b51dfa87991ebcdb7a0af0e0ddd1e
clock <clock@wisc.edu>
2026-01-14 09:56:47 -0600

    Add indexes to speed up queries

--- transition/post-script.sql
+++ transition/post-script.sql
@@ -260,3 +260,16 @@ CREATE VIEW user_submit_nodes AS
         s.name as submit_node_name
     FROM user_submits us
     JOIN submit_nodes s ON us.submit_node_id = s.id;
+
+-- Indexes --
+
+CREATE INDEX CONCURRENTLY idx_user_notes_userid_projectid_noteid
+  ON user_notes (user_id, project_id, note_id);
+
+-- Also a specialized index for fetching the latest note by user quickly:
+CREATE INDEX CONCURRENTLY idx_user_notes_userid_noteid_desc
+  ON user_notes (user_id, note_id DESC);
+
+CREATE INDEX CONCURRENTLY idx_user_submits_userid_submitnodeid_incl
+  ON user_submits (user_id, submit_node_id)
+  INCLUDE (disk_quota, hpc_diskquota, hpc_inodequota, hpc_joblimit, hpc_corelimit, hpc_fairshare);
