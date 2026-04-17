4273af9d0726d8109d0ffaabfdde8f7226ccb149
fallow64 <fallow640@gmail.com>
2026-04-17 15:49:48 -0500

    Add templates to form submission and form approval

--- userapp/api/routes/forms/_util.py
+++ userapp/api/routes/forms/_util.py
@@ -9,7 +9,46 @@ from userapp.core.schemas.user_application_form import UserFormPatch
 from userapp.core.schemas.user_project import UserProjectTableSchema
 from userapp.api.routes._util import _patch_user_submit_nodes
 
-async def on_user_form_submit(session: AsyncSession, form_id: int, form: UserFormTable | None) -> None:
+ON_USER_FORM_SUBMIT_EMAIL_TEMPLATE = """
+Dear {name},
+
+Thank you for requesting an account at CHTC! The CHTC Facilitation Staff will reach out to you within 2-3 business days. Contact us at chtc@cs.wisc.edu if you don't hear back from us after 3 business days.
+
+Best,
+The CHTC Research Computing Facilitation team
+""".strip()
+
+ON_USER_FORM_APPROVAL_EMAIL_TEMPLATE = """
+Dear {name},
+
+Thanks for getting in touch with the Center for High Throughput Computing (CHTC)! Based on your application form, we have created your account on our High Throughput Computing (HTC) system and on our High Performance Computing (HPC) system. It will be ready to use within the next couple hours. To log in, use your campus NetID and password to SSH into the login servers.
+
+For HTC:
+• Use ap2001.chtc.wisc.edu
+
+For HPC:
+• Use spark-login.chtc.wisc.edu
+
+You will need to be on the campus internet or VPN to access the servers.
+
+For information on how to get started, visit the following pages of our website:
+• How to log in: https://chtc.cs.wisc.edu/uw-research-computing/connecting
+• How to prepare and submit HTC jobs: https://chtc.cs.wisc.edu/uw-research-computing/htcondor-job-submission
+• How to prepare and submit HPC jobs: https://chtc.cs.wisc.edu/uw-research-computing/hpc-job-submission
+
+We have many guides on our website to help you with a variety of topics, including how to set up your software, organize job submissions, and handle large data: https://chtc.cs.wisc.edu/uw-research-computing/htc/guides.html and https://chtc.cs.wisc.edu/uw-research-computing/hpc/guides.html
+
+CHTC’s mission is to accelerate research by providing access to computing and data capacity and to provide personalized support to individual researchers. After reviewing the materials above, please schedule an initial consultation with us to answer any questions you may have about getting started by using this link: go.wisc.edu/schedule-chtc. This could be right away, if you’re not sure where to start, or after a few weeks of experimentation. It’s up to you!
+
+You can also reach out for support by emailing chtc@cs.wisc.edu or stopping by our Zoom office hours on Tuesdays 10:30 AM - 12:00 PM and Thursdays 3:00 PM - 4:30 PM, online at go.wisc.edu/chtc-officehours. Active system issues are reported via our status page at https://status.chtc.wisc.edu.
+
+Please don’t hesitate to reach out with any questions you have about getting started!
+
+Best,
+The CHTC Research Computing Facilitation team
+""".strip()
+
+async def on_user_form_submit(session: AsyncSession, form_id: int, form: None) -> None:
     user_form = await session.scalar(
         select(UserFormTable)
         .where(UserFormTable.id == form_id)
@@ -18,8 +57,9 @@ async def on_user_form_submit(session: AsyncSession, form_id: int, form: UserFor
 
     # Try sending the user an email
     try:
-        send_email("chtc@cs.wisc.edu", user.email1, "CHTC Account Application Received", "Thank you for requesting an account at CHTC! The CHTC Facilitation Staff will reach out to you within 2-3 business days. Contact us at chtc@cs.wisc.edu if you don't hear back from us after 3 business days.")
-    except:
+        text = ON_USER_FORM_SUBMIT_EMAIL_TEMPLATE.format(name=user.name or "user")
+        send_email("chtc@cs.wisc.edu", user.email1, "CHTC Account Application Received", text)
+    except Exception:
         # we don't care if the email send fails
         pass
 
@@ -67,6 +107,14 @@ async def on_user_form_accept(session: AsyncSession, form_id: int, form: UserFor
         )
         # Update to the set of approved submit nodes
         await _patch_user_submit_nodes(session, user, form.submit_nodes)
+    
+    # Try sending the user an email
+    try:
+        text = ON_USER_FORM_APPROVAL_EMAIL_TEMPLATE.format(name=user.name or "user")
+        send_email("chtc@cs.wisc.edu", user.email1, "CHTC Account Application Approved", text)
+    except Exception:
+        # we don't care if the email send fails
+        pass
 
 
 async def _clear_user_projects(session: AsyncSession, user) -> None:
